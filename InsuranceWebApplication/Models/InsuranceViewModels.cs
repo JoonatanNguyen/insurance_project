37f07ef0826630eefc49bea0df33d8dbb3f919bd
Sven Slijkoord <sven.slijkoord@gmail.com>
2018-10-10 15:03:30 +0300

    Added create insurance claim frontend form scaffolding

--- InsuranceWebApplication/Models/InsuranceViewModels.cs
+++ InsuranceWebApplication/Models/InsuranceViewModels.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace InsuranceWebApplication.Models
+﻿namespace InsuranceWebApplication.Models
 {
-    public class InsuranceViewModels
+    public class InsuranceClaimViewModel
     {
-        public class InsuranceClaimViewModel
-        {
-            public string Name { get; set; }
-            public string Gender { get; set; }
-            public string PhoneNumber { get; set; }
-            public string Email { get; set; }
-            public string Address { get; set; }
-            public string Description { get; set; }
-        }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string Description { get; set; }
     }
 }
