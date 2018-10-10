530e853682f1b21de0a9ed97e318f4fb482739f4
Sven Slijkoord <sven.slijkoord@gmail.com>
2018-10-10 14:53:24 +0300

    Added base insurance claim model

---
+++ InsuranceWebApplication/Models/InsuranceViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceWebApplication.Models
+{
+    public class InsuranceViewModels
+    {
+        public class InsuranceClaimViewModel
+        {
+            public string Name { get; set; }
+            public string Gender { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string Address { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
