e01d2eaea80aa46ae98bb81cd105cd723ff4ccd4
Minh Nguyen <minhnguyen181199@gmail.com>
2018-10-31 17:02:54 +0200

    add customer form

---
+++ InsuranceWebApplication/Models/CustomerViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceWebApplication.Models
+{
+    public class CustomerViewModels
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
