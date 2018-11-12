82857320344c12099a48e24a38439a9dfea019f3
Minh Nguyen <minhnguyen181199@gmail.com>
2018-11-12 13:52:05 +0200

    CustomerFormChanges

--- InsuranceWebApplication/Models/CustomerViewModels.cs
+++ InsuranceWebApplication/Models/CustomerViewModels.cs
@@ -10,6 +10,7 @@ namespace InsuranceWebApplication.Models
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Password { get; internal set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
     }
 }
